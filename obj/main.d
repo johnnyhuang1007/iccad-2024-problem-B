523539694aace67388f324a94bb3ccd257e6982d
johnnyhuang1007 <johnnyhuang1007@gmail.com>
2024-09-05 01:40:34 +0800

    GREAT PROGRESS!

--- obj/main.d
+++
@@ -1,3 +0,0 @@
-obj/main.d: src/main.cpp src/FlowFunc.hpp include/Plane.h \
- include/module.h include/corner_stitch.h include/PlacerInOut.h \
- include/Plane_extension.h include/Plane.h include/module_extension.h
--- obj/optimatization_bin.d
+++
@@ -1,3 +0,0 @@
-obj/optimatization_bin.d: src/optimatization_bin.cpp \
- include/Plane_extension.h include/Plane.h include/module.h \
- include/corner_stitch.h include/module_extension.h
